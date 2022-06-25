33567ffc68e0f76b48ddb5ff20672e60df9ed83b
SaidNivek <kevin.vi.dias@gmail.com>
2022-06-25 12:00:40 -0400

    Begin about section layout and content, no images

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './About.scss'
+import { motion } from 'framer-motion'
+
+const abouts = [
+  { 
+    title: "Web Development",
+    description: "I am a good web developer",
+    imgUrl: ''
+  },
+  { 
+    title: "Independent Learner",
+    description: "Able to quickly and competently learn and then assimilate new languages, frameworks, and libraries",
+    imgUrl: ''
+  },
+  { 
+    title: "Motivated Team Member",
+    description: "Communicative, nurturing, and reliable in group settings",
+    imgUrl: ''
+  }
+]
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <h2 className="head-text">I Know That 
+      <span>Good Design</span>
+      <br />
+      means
+      <span>Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}  
+            className="app__profile-item"
+            key={about.title + index}    
+          >
+            <img src={about.imgUrl} alt ={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+          </motion.div>
+        ))}
+      </div>
+    </>
   )
 }
 
