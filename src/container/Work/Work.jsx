21bf8d4a1ea1dd858f7ac70bbcce5e582a725aeb
SaidNivek <kevin.vi.dias@gmail.com>
2022-06-29 17:59:29 -0400

    Work section begin, add useState and HandleWorkFilter

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Work.scss'
+import { AiFillEye, AiFillGithub } from 'react-icons/ai'
+import { motion } from 'framer-motion'
+import { AppWrap } from '../../wrapper'
+import { urlFor, client } from '../../client'
+
 
 const Work = () => {
+
+  const [activeFilter, setActiveFilter] = useState('All')
+
+  const handleWorkFilter = (item) => {
+
+  }
+
   return (
-    <div>Work</div>
+    <>
+      <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
+
+      <div className="app__work-filter">
+        {['UI/UX', 'Web App', 'Mobile App', 'React JSON', 'All'].map((item, index) => (
+          <div 
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? `item-active` : ''}`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
 
