713c19e7702cf43794237ad97f4314270c4cbbe9
SaidNivek <kevin.vi.dias@gmail.com>
2022-07-05 13:07:00 -0400

    Fix typo, change navdots color to dark teal

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const NavigationDots = ({ active }) => {
   return (
     <div className="app__navigation">
-        {['home', 'about', 'work', 'skills', 'tesitmonials', 'contact'].map((item, index) => (
+        {['home', 'about', 'work', 'skills', 'testimonials', 'contact'].map((item, index) => (
             <a 
                 href={`#${item}`}
                 key={item + index}
                 className="app__navigation-dot"
-                style={active === item ? { backgroundColor: '#313bac'} : {}}
+                style={active === item ? { backgroundColor: '#2C5757'} : {}}
             />
         ))}
     </div>
