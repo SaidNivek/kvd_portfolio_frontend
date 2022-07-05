61084194d80dfeecd3f2dcff0da1cd74e3903b1a
SaidNivek <kevin.vi.dias@gmail.com>
2022-07-05 12:45:39 -0400

    Remove pictures from testimonial section

--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -37,7 +37,7 @@ const Testimonial = () => {
       {testimonials.length && (
         <>
         <div className="app__testimonial-item app__flex">
-          <img src={urlFor(test.imgurl)} alt="testimonial" />
+          {/* <img src={urlFor(test.imgurl)} alt="testimonial" /> */}
           <div className="app__testimonial-content">
             <p className="p-text">{test.feedback}</p>
             <div>
